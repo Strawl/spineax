7ed3620d0e03c11514dc83d890868c68aead3748
Dominik Gyarmati <dominik.gyarmati@gmail.com>
2026-01-13 23:40:15 +0100

    Build pypi wheels with github actions

---
+++ scripts/build.sh
@@ -0,0 +1,2 @@
+export CMAKE_ARGS="-DPython_ROOT_DIR=/opt/python/cp310-cp310 -DPython_EXECUTABLE=/opt/python/cp310-cp310/bin/python"
+/opt/python/cp310-cp310/bin/pyproject-build --installer uv --wheel
